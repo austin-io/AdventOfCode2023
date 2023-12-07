1c7aa8da0eae6297ad466b95dd14528a57407269
Austin Poberezhnik <apoberezhnik23@gmail.com>
2023-12-07 07:14:58 -0800

    AOC day 7 part 2

--- day7/main.nim
+++ day7/main.nim
@@ -18,6 +18,22 @@ const CARD_MAP = toTable[char, int]([
     ('A', 13),
 ])
 
+const CARD_MAP_WILDCARD = toTable[char, int]([
+    ('2', 1),
+    ('3', 2),
+    ('4', 3),
+    ('5', 4),
+    ('6', 5),
+    ('7', 6),
+    ('8', 7),
+    ('9', 8),
+    ('T', 9),
+    ('J', 0), # Joker is weakest
+    ('Q', 11),
+    ('K', 12),
+    ('A', 13),
+])
+
 type 
     HandType = enum
         HighCard = 1, OnePair, TwoPair, ThreeKind, FullHouse, FourKind, FiveKind
@@ -63,11 +79,58 @@ proc GetHandType(handData: HandData) : HandType =
         else:
             return HighCard
 
+proc GetHandTypeWithWildCard(handData: HandData) : HandType =
+
+    var cardMap : Table[char, int]
+    var highest = 0
+    var highestChar : char 
+    var twoCount = 0
+    var jokerCount = 0
+
+    for card in handData.hand:
+        if(card == 'J'): jokerCount+=1
+        if(cardMap.hasKey(card)):
+            cardMap[card] += 1
+            continue
+
+        cardMap[card] = 1
+    
+    for k,cardCount in cardMap:
+        if(k == 'J'): continue
+        if(cardCount > highest):
+            highest = cardCount
+            highestChar = k
+    
+    if(highest > 0):
+        cardMap[highestChar] += jokerCount
+
+    highest += jokerCount
+
+    for k,cardCount in cardMap:
+        if(cardCount == 2 and k != 'J'):
+            twoCount += 1
+
+    case highest:
+        of 5:
+            return FiveKind
+        of 4:
+            return FourKind
+        of 3:
+            if(twoCount == 1):
+                return FullHouse
+            return ThreeKind
+        of 2:
+            if(twoCount == 2):
+                return TwoPair
+            return OnePair
+        else:
+            return HighCard
+
 # return value:
 # -1 -> h1 is greater
 #  0 -> equal
 #  1 -> h2 is greater
-proc CompareHands(h1, h2 : HandData) : int =
+proc CompareHands(h1, h2 : HandData, cardMap: Table[char, int]) : int =
 
     if(h1.hType > h2.hType):
         return -1
@@ -75,8 +138,8 @@ proc CompareHands(h1, h2 : HandData) : int =
         return 1
 
     for i in 0..h1.hand.high:
-        var h1Strength = CARD_MAP[h1.hand[i]]
-        var h2Strength = CARD_MAP[h2.hand[i]]
+        var h1Strength = cardMap[h1.hand[i]]
+        var h2Strength = cardMap[h2.hand[i]]
 
         if(h1Strength == h2Strength): continue
 
@@ -87,7 +150,6 @@ proc CompareHands(h1, h2 : HandData) : int =
 
 proc Part1() =
     var lines = readFile("input.txt").splitLines
-    echo lines
 
     var hands : seq[HandData] = @[]
     var totalWinnings = 0
@@ -98,7 +160,7 @@ proc Part1() =
         newHand.hType = GetHandType(newHand)
         hands.add(newHand)
     
-    hands.sort(proc (h1, h2: HandData) : int = return CompareHands(h1, h2))
+    hands.sort(proc (h1, h2: HandData) : int = return CompareHands(h1, h2, CARD_MAP))
 
     for i in 0..hands.high:
         var currentHand = hands[hands.high-i]
@@ -108,4 +170,27 @@ proc Part1() =
     echo hands
     echo totalWinnings
 
+proc Part2() =
+    var lines = readFile("input.txt").splitLines
+
+    var hands : seq[HandData] = @[]
+    var totalWinnings = 0
+
+    for line in lines:
+        var lineSplit = line.splitWhitespace
+        var newHand = HandData(hand: lineSplit[0], bid: lineSplit[1].parseInt)
+        newHand.hType = GetHandTypeWithWildCard(newHand)
+        hands.add(newHand)
+    
+    hands.sort(proc (h1, h2: HandData) : int = return CompareHands(h1, h2, CARD_MAP_WILDCARD))
+
+    for i in 0..hands.high:
+        hands[hands.high-i].rank = i+1
+        var currentHand = hands[hands.high-i]
+        totalWinnings += currentHand.rank * currentHand.bid
+
+    echo hands
+    echo totalWinnings
+
 Part1()
+Part2()
